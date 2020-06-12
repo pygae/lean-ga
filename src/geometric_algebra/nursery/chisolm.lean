cb201326de6bf6f2db528a5526cafe4b658af10f
utensil <utensilcandel@gmail.com>
2020-06-12 14:22:53 +0800

    Got hom working

--- src/geometric_algebra/nursery/chisolm.lean
+++ src/geometric_algebra/nursery/chisolm.lean
@@ -2,40 +2,51 @@
 Copyright (c) 2020 Utensil Song. All rights reserved.
 Released under MIT license as described in the file LICENSE.
 Authors: Utensil Song
--/
 
-import tactic.ring_exp ring_theory.algebra algebra.opposites algebra.commute data.equiv.ring
-import linear_algebra.quadratic_form
+This file is based on https://arxiv.org/abs/1205.5935
+-/
+import algebra.group.hom
+import ring_theory.algebra
 import data.real.basic
 import data.complex.basic
-import analysis.normed_space.real_inner_product
-import geometry.manifold.real_instances
 
 universes u u₀ u₁
 
-class geometric_algebra (G : Type u)
+class geometric_algebra (G : Type*)
 -- Axiom 2: G contains a field G0 of characteristic zero which includes 0 and 1.
-(G₀ : set G) [field G₀] [char_zero G₀] [has_zero G₀] [has_one G₀]
+(G₀ : Type*) [field G₀]
 -- Axiom 3: G contains a subset G1 closed under addition, and λ ∈ G0, v ∈ G1 implies λv = vλ ∈ G1.
-(G₁ : set G) [add_comm_group G₁] [vector_space G₀ G₁]
+(G₁ : Type*) [add_comm_group G₁] [vector_space G₀ G₁]
 -- Axiom 1: G is a ring with unit. 
 -- The additive identity is called 0 and the multiplicative identity is called 1.
 [ring G]
-extends G₁ →+ G
+-- TODO: find better ways to map G₀ and G₁ to G
+-- [has_coe G₀ G] [has_coe G₁ G]
+-- [G₀ →+* G]
+-- [algebra G₀ G]
 :=
+(scalar_hom : G₀ →+* G)
+(vec_hom : G₁ →+ G)
 -- Axiom 4: The square of every vector is a scalar.
-(vec_sq_scalar : ∀ v : G₁, (v * v : G) ∈ G₀)
+(vec_sq_scalar : ∀ v : G₁, ∃ k : G₀, vec_hom(v) * vec_hom(v) = scalar_hom(k) )
 
 namespace geometric_algebra
 
 section
 
-variables (G : Type u)
-(G₀ : set G) [field G₀] [char_zero G₀] [has_zero G₀] [has_one G₀]
-(G₁ : set G) [add_comm_group G₁] [vector_space G₀ G₁]
+variables  (G : Type*)
+-- Axiom 2: G contains a field G0 of characteristic zero which includes 0 and 1.
+(G₀ : Type*) [field G₀] [char_zero G₀]
+-- Axiom 3: G contains a subset G1 closed under addition, and λ ∈ G0, v ∈ G1 implies λv = vλ ∈ G1.
+(G₁ : Type*) [add_comm_group G₁] [vector_space G₀ G₁]
+-- Axiom 1: G is a ring with unit. 
+-- The additive identity is called 0 and the multiplicative identity is called 1.
 [ring G]
-[algebra G₀ G]
-[geometric_algebra G G₀ G₁]
+-- TODO: find better ways to map G₀ and G₁ to G
+-- [has_coe G₀ G] [has_coe G₁ G]
+-- [G₀ →+* G]
+-- [algebra G₀ G]
+[GA : geometric_algebra G G₀ G₁]
 
 lemma assoc : ∀ A B C : G, (A * B) * C = A * (B * C) := λ A B C, semigroup.mul_assoc A B C
 
@@ -51,78 +62,94 @@ local postfix `²`:80 := λ a : G, a * a
 
 local infix `*₊`:75 := λ a b : G, a * b + b * a
 
-lemma vec_sym_prod_scalar [geometric_algebra G G₀ G₁] :
-∀ (a b : G₁), a *₊ b ∈ G₀ :=
+-- def square {X : Type*} [has_coe X G] (A : X) : G := A * A
+
+-- def sym_prod {X : Type*} [has_coe X G] (A B : X) : G := A * B + B * A
+
+-- local infix `*₊`:75 := sym_prod
+
+-- local postfix `²`:80 := square
+
+/-
+  Symmetrised product of two vectors must be a scalar
+-/
+lemma vec_sym_prod_scalar :
+∀ (a b : G₁), ∃ k : G₀, a *₊ b = (k : G) :=
 assume a b,
 have h1 : (a + b)² = (a² + b² + a *₊ b : G), from begin
-    simp only [],
-    rw distrib.left_distrib (↑a + ↑b) ↑a ↑b,
-    rw distrib.right_distrib _ _ ↑a,
-    rw distrib.right_distrib _ _ ↑b,
-    cc,
+  repeat {rw square},
+  sorry
+  -- G : Type u_1,
+  -- G₀ : Type u_2,
+  -- G₁ : Type u_3,
+  -- _inst_1 : field G₀,
+  -- _inst_2 : has_coe G₀ G,
+  -- _inst_3 : add_comm_group G₁,
+  -- _inst_4 : vector_space G₀ G₁,
+  -- _inst_5 : has_coe G₁ G,
+  -- _inst_6 : ring G,
+  -- _inst_7 : algebra G₀ G,
+  -- _inst_8 : geometric_algebra G G₀ G₁,
+  -- a b : G₁
+  -- ⊢ ↑(a + b) * ↑(a + b) = ↑a * ↑a + ↑b * ↑b + a*₊b
+  -- repeat {unfold coe, unfold lift_t, unfold has_lift_t.lift, unfold coe_t, unfold has_coe_t.coe, unfold coe_b, unfold has_coe.coe},
 end,
-have h2 : a *₊ b = ((a + b)² - a² - b² : G), from
-begin
-    apply_fun (λ x, - (a² + b²) + x) at h1,
-    simp only [],
-    noncomm_ring,
-end,
-have vec_sq_scalar : ∀ v : G₁, v² ∈ G₀, from
+have h2 : a *₊ b = ((a + b)² - a² - b² : G), from by sorry,
+have vec_sq_scalar :
+          ∀ v : G₁, ∃ k : G₀, (v² : G) = (k : G), from
 begin
   intro v,
   apply geometric_algebra.vec_sq_scalar (v),
   repeat {assumption},
 end,
-have hl : has_lift_t ↥G₁ ↥G₁, from by sorry,
-have h3 : (↑a : G)² ∈ G₀, from by apply vec_sq_scalar,
-have h4 : (↑b : G)² ∈ G₀, from by apply vec_sq_scalar,
-have h5 : (a + b : G) ∈ G₁ , from
-begin
-    -- rw add_monoid_hom.map_add',
-    -- rewrite tactic failed, did not find instance of the pattern in the target expression
-    --   ?m_5.to_fun (?m_6 + ?m_7)
-    -- 1 goal
-    -- G : Type u,
-    -- G₀ : set G,
-    -- _inst_1 : field ↥G₀,
-    -- _inst_2 : char_zero ↥G₀,
-    -- _inst_3 : has_zero ↥G₀,
-    -- _inst_4 : has_one ↥G₀,
-    -- G₁ : set G,
-    -- _inst_5 : add_comm_group ↥G₁,
-    -- _inst_6 : vector_space ↥G₀ ↥G₁,
-    -- _inst_7 : ring G,
-    -- _inst_8 : algebra ↥G₀ G,
-    -- _inst_9 _inst_10 : geometric_algebra G G₀ G₁,
-    -- a b : ↥G₁,
-    -- h1 :
-    --   (λ (a : G), a * a) (↑a + ↑b) =
-    --     (λ (a : G), a * a) ↑a + (λ (a : G), a * a) ↑b + (λ (a b : G), a * b + b * a) ↑a ↑b,
-    -- h2 :
-    --   (λ (a b : G), a * b + b * a) ↑a ↑b =
-    --     (λ (a : G), a * a) (↑a + ↑b) - (λ (a : G), a * a) ↑a - (λ (a : G), a * a) ↑b,
-    -- vec_sq_scalar : ∀ (v : ↥G₁), (λ (a : G), a * a) ↑v ∈ G₀,
-    -- hl : has_lift_t ↥G₁ ↥G₁,
-    -- h3 : (λ (a : G), a * a) ↑a ∈ G₀,
-    -- h4 : (λ (a : G), a * a) ↑b ∈ G₀
-    -- ⊢ ↑a + ↑b ∈ G₁
-    sorry
-end,
-have h6 : (↑a + ↑b)² ∈ G₀, from
-begin
-    simp only [],
-    simp only [] at vec_sq_scalar,
-    sorry
-end,
-begin
-    simp only [] at h2,
-    simp only [],
-    rw h2,
-    sorry
-    --apply vec_sq_scalar (↑a),
-end
+exists.elim (vec_sq_scalar (a + b))
+(
+  assume kab,
+  exists.elim (vec_sq_scalar a)
+  (
+    assume ka,
+    exists.elim (vec_sq_scalar b)
+    (
+      assume kb,
+      begin
+        intros hb ha hab,
+        rw h2,
+        use kab - ka - kb,
+        simp only [],
+        simp only [] at h1,
+        simp only [] at h2,
+        simp only [] at ha,
+        simp only [] at hb,
+        simp only [] at hab,
+        rw [hb, ha],
+        
+        -- 1 goal
+        -- G : Type u_1,
+        -- G₀ : Type u_2,
+        -- G₁ : Type u_3,
+        -- _inst_1 : field G₀,
+        -- _inst_2 : has_coe G₀ G,
+        -- _inst_3 : add_comm_group G₁,
+        -- _inst_4 : vector_space G₀ G₁,
+        -- _inst_5 : has_coe G₁ G,
+        -- _inst_6 : ring G,
+        -- _inst_7 : algebra G₀ G,
+        -- _inst_8 : geometric_algebra G G₀ G₁,
+        -- a b : G₁,
+        -- h1 : (a + b)² = a² + b² + a*₊b,
+        -- h2 : a*₊b = (a + b)² - a² - b²,
+        -- vec_sq_scalar : ∀ (v : G₁), ∃ (k : G₀), v² = ↑k,
+        -- kab ka kb : G₀,
+        -- hb : b² = ↑kb,
+        -- ha : a² = ↑ka,
+        -- hab : (a + b)² = ↑kab
+        -- ⊢ ↑kab - ↑ka - ↑kb = ↑(kab - ka - kb)
+        sorry
+      end
+    )
+  )
+)
 
 end
 
 end geometric_algebra
-
