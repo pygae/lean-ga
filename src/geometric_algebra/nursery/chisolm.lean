35da4351c1c778bfd33e6c4d836f2aca2c04167c
utensil <utensilcandel@gmail.com>
2020-06-13 14:18:06 +0800

    Fix the issue

--- src/geometric_algebra/nursery/chisolm.lean
+++ src/geometric_algebra/nursery/chisolm.lean
@@ -12,41 +12,33 @@ import data.complex.basic
 
 universes u u₀ u₁
 
-class geometric_algebra (G : Type*)
+class geometric_algebra
 -- Axiom 2: G contains a field G0 of characteristic zero which includes 0 and 1.
 (G₀ : Type*) [field G₀]
--- Axiom 3: G contains a subset G1 closed under addition, and λ ∈ G0, v ∈ G1 implies λv = vλ ∈ G1.
+-- Axiom 3: G contains a subset G1 closed under addition, 
+-- and λ ∈ G0, v ∈ G1 implies λv = vλ ∈ G1.
 (G₁ : Type*) [add_comm_group G₁] [vector_space G₀ G₁]
 -- Axiom 1: G is a ring with unit. 
 -- The additive identity is called 0 and the multiplicative identity is called 1.
-[ring G]
--- TODO: find better ways to map G₀ and G₁ to G
--- [has_coe G₀ G] [has_coe G₁ G]
--- [G₀ →+* G]
--- [algebra G₀ G]
+(G : Type*) [ring G]
+[algebra G₀ G]
 :=
-(f₀ : G₀ →+* G)
 (f₁ : G₁ →+ G)
 -- Axiom 4: The square of every vector is a scalar.
-(vec_sq_scalar : ∀ v : G₁, ∃ k : G₀, f₁ v * f₁ v = f₀ k )
+(vec_sq_scalar : ∀ v : G₁, ∃ k : G₀, f₁ v * f₁ v = algebra_map _ _ k )
 
 namespace geometric_algebra
 
 section
 
-variables  (G : Type*)
--- Axiom 2: G contains a field G0 of characteristic zero which includes 0 and 1.
-(G₀ : Type*) [field G₀] [char_zero G₀]
--- Axiom 3: G contains a subset G1 closed under addition, and λ ∈ G0, v ∈ G1 implies λv = vλ ∈ G1.
-(G₁ : Type*) [add_comm_group G₁] [vector_space G₀ G₁]
--- Axiom 1: G is a ring with unit. 
--- The additive identity is called 0 and the multiplicative identity is called 1.
-[ring G]
--- TODO: find better ways to map G₀ and G₁ to G
--- [has_coe G₀ G] [has_coe G₁ G]
--- [G₀ →+* G]
--- [algebra G₀ G]
-[GA : geometric_algebra G G₀ G₁]
+parameters
+{G₀ : Type*} [field G₀]
+{G₁ : Type*} [add_comm_group G₁] [vector_space G₀ G₁]
+{G : Type*} [ring G] [algebra G₀ G] [geometric_algebra G₀ G₁ G]
+
+def fᵥ : G₁ →+ G := f₁ G₀
+
+def fₛ : G₀ →+* G := algebra_map G₀ G
 
 lemma assoc : ∀ A B C : G, (A * B) * C = A * (B * C) := λ A B C, semigroup.mul_assoc A B C
 
@@ -58,27 +50,19 @@ def square (a : G) := a * a
 
 def sym_prod (a b : G) := a * b + b * a
 
--- local postfix `²`:80 := λ a : G, a * a
-
--- local infix `*₊`:75 := λ a b : G, a * b + b * a
-
--- def square {X : Type*} [has_coe X G] (A : X) : G := A * A
-
--- def sym_prod {X : Type*} [has_coe X G] (A B : X) : G := A * B + B * A
+def sym_prod_vec (a b : G₁) := sym_prod (fᵥ a) (fᵥ b)
 
 local infix `*₊`:75 := sym_prod
 
-local postfix `²`:80 := square
+local infix `*₊ᵥ`:75 := sym_prod_vec
 
-#check f₀
+local postfix `²`:80 := square
 
 /-
   Symmetrised product of two vectors must be a scalar
 -/
-lemma vec_sym_prod_scalar :
-∀ (a b : G₁), ∃ k : G₀, 
-  f₁ a *₊ f₁ b = f₀ k := by sorry
-
+lemma vec_sym_prod_scalar:
+∀ (a b : G₁), ∃ k : G₀, a *₊ᵥ b = fₛ k := by sorry
 
 end
 
