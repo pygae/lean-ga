27edad5997dd7cb82a0c00534ae57ba152b21acd
utensil <utensilcandel@gmail.com>
2020-07-13 01:25:41 +0800

    Add inspirations.lean

---
+++ src/geometric_algebra/generic/inspirations.lean
@@ -0,0 +1,33 @@
+/-
+Copyright (c) 2020 lean-ga Team. All rights reserved.
+Released under MIT license as described in the file LICENSE.
+Authors: Utensil Song
+
+This file contains only imports which are just the lean files
+that the authors draw inspirations from.
+-/
+import init.core
+
+import algebra.group.defs
+import algebra.group.hom
+import algebra.module
+import algebra.direct_limit
+import ring_theory.algebra
+import linear_algebra.quadratic_form
+
+import data.real.basic
+import data.complex.basic
+import data.complex.module
+import data.matrix.basic
+
+import geometry.euclidean
+import geometry.manifold.real_instances
+
+import analysis.convex.cone
+import analysis.normed_space.basic
+import analysis.normed_space.real_inner_product
+
+import tactic
+import tactic.lint
+
+#lint
