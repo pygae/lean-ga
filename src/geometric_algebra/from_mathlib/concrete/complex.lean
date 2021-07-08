db26956b0ac10aff444c6b861ae3150611a40eff
Eric Wieser <wieser.eric@gmail.com>
2021-07-08 12:27:50 +0100

    It turns out that ring operations on the reals are computable after all (after a contribution to mathlib to fix this)

--- src/geometric_algebra/from_mathlib/concrete/complex.lean
+++ src/geometric_algebra/from_mathlib/concrete/complex.lean
@@ -12,7 +12,7 @@ import linear_algebra.clifford_algebra.basic
 namespace clifford_algebra_complex
 
 /-- The quadratic form sending elements to the negation of their square. -/
-noncomputable def Q : quadratic_form ℝ ℝ :=
+def Q : quadratic_form ℝ ℝ :=
 -quadratic_form.lin_mul_lin linear_map.id linear_map.id
 
 @[simp]
@@ -20,7 +20,7 @@ lemma Q_apply (r : ℝ) : Q r = - (r * r) := rfl
 
 /-- Intermediate result of `clifford_algebra_complex.equiv_complex`: clifford algebras over
 `clifford_algebra_complex.Q` above can be converted to `ℂ`. -/
-noncomputable def to_complex : clifford_algebra Q →ₐ[ℝ] ℂ :=
+def to_complex : clifford_algebra Q →ₐ[ℝ] ℂ :=
 clifford_algebra.lift Q ⟨linear_map.to_span_singleton _ _ complex.I, λ r, begin
   dsimp [linear_map.to_span_singleton, linear_map.id],
   rw smul_mul_smul,
@@ -33,7 +33,7 @@ clifford_algebra.lift_ι_apply _ _ r
 
 /-- The clifford algebras over `clifford_algebra_complex.Q` is isomorphic as an `ℝ`-algebra to `ℂ`. -/
 @[simps]
-noncomputable def equiv_complex : clifford_algebra Q ≃ₐ[ℝ] ℂ :=
+def equiv_complex : clifford_algebra Q ≃ₐ[ℝ] ℂ :=
 alg_equiv.of_alg_hom to_complex
   (complex.lift ⟨clifford_algebra.ι Q 1, begin
     rw [clifford_algebra.ι_sq_scalar, Q_apply, one_mul, ring_hom.map_neg, ring_hom.map_one],
