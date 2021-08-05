5fe18abe8e456ef8972c29312e43727952fd98b7
Eric Wieser <wieser.eric@gmail.com>
2021-08-05 15:01:28 +0100

    Add a file to ensure the links work

---
+++ src/geometric_algebra/from_mathlib/concrete/default.lean
@@ -0,0 +1,18 @@
+/-
+Copyright (c) 2021 Eric Wieser, Utensil Song. All rights reserved.
+Released under MIT license as described in the file LICENSE.
+Authors: Eric Wieser
+-/
+
+-- this is needed for our doc-gen instance to find these definitions
+import linear_algebra.clifford_algebra.equivs
+
+/-!
+# Specific algebras
+
+This folder contains "concrete" instantiations of algebras of interest. Note that some of
+the algebras in this folder have graduated to mathlib, such as:
+
+* `clifford_algebra_ring.equiv`: any ring is a clifford algebra
+* `clifford_algebra_complex.equiv`: the complex numbers are a clifford algebra
+-/
