cde588fda785856025ff9411a51d68403e2fc3ae
Eric Wieser <wieser.eric@gmail.com>
2022-02-05 14:10:23 +0100

    One direction of the equivalence between the N-algebra and the even subalgebra of the N+1-algebra

--- src/geometric_algebra/from_mathlib/even_odd.lean
+++ src/geometric_algebra/from_mathlib/even_odd.lean
@@ -45,4 +45,61 @@ def even : subalgebra R (clifford_algebra Q) :=
 lemma even_to_submodule : (even Q).to_submodule = even_odd Q 0 :=
 rfl
 
+
+namespace equiv_even
+
+@[reducible]
+def Q' := (Q.prod $ -@quadratic_form.sq R _)
+
+/-- The unit vector in the new dimension -/
+def e0 := ι (Q' Q) (0, 1)
+
+lemma e0_mul_e0 : e0 Q * e0 Q = -1 :=
+(ι_sq_scalar _ _).trans begin
+  simp,
+end
+
+lemma neg_e0_mul_ι (m : M) : -(e0 Q * ι _ (m, 0)) = ι _ (m, 0) * e0 Q :=
+begin
+  refine neg_eq_of_add_eq_zero ((ι_mul_ι_add_swap _ _).trans _),
+  dsimp [quadratic_form.polar],
+  simp only [add_zero, mul_zero, mul_one, zero_add, neg_zero, quadratic_form.map_zero,
+    add_sub_cancel, sub_self, map_zero, zero_sub],
+end
+
+lemma neg_ι_mul_e0 (m : M) : -(ι _ (m, 0) * e0 Q) = e0 Q * ι _ (m, 0) :=
+begin
+  rw neg_eq_iff_neg_eq,
+  exact neg_e0_mul_ι _ m
+end
+
+#check tactic.rewrite_cfg
+
+/-- The embedding from the smaller algebra into the new larger one. -/
+def to_even : clifford_algebra Q →ₐ[R] clifford_algebra.even (Q' Q) :=
+begin
+  refine clifford_algebra.lift Q ⟨_, λ m, _⟩,
+  { refine linear_map.cod_restrict _ _ (λ m, submodule.mem_supr_of_mem ⟨2, rfl⟩ _),
+    exact (algebra.lmul_left R $ e0 Q).comp ((ι _).comp $ linear_map.inl R M R),
+    rw [subtype.coe_mk, pow_two],
+    exact submodule.mul_mem_mul (linear_map.mem_range_self _ _) (linear_map.mem_range_self _ _), },
+  { ext1,
+    dsimp only [subalgebra.coe_mul, linear_map.cod_restrict_apply, linear_map.comp_apply,
+      algebra.lmul_left_apply, linear_map.inl_apply, subalgebra.coe_algebra_map],
+    rw [←neg_ι_mul_e0] {occs := occurrences.pos [1]},
+    rw [neg_mul_eq_mul_neg, mul_assoc, ←mul_assoc _ (e0 _), neg_mul_eq_neg_mul_symm, e0_mul_e0,
+      neg_neg, one_mul, ι_sq_scalar],
+    dsimp [Q'],
+    rw [zero_mul, neg_zero, add_zero], }
+end
+
+@[simp]
+lemma to_even_ι (m : M) : (to_even Q (ι Q m) : clifford_algebra (Q' Q)) = e0 Q * ι (Q' Q) (m, 0) :=
+begin
+  rw [to_even, clifford_algebra.lift_ι_apply, linear_map.cod_restrict_apply],
+  refl,
+end
+
+end equiv_even
+
 end clifford_algebra
