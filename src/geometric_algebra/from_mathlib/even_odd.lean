22fda10513bccdf109b79d63f492a53f6bfbb3f0
Eric Wieser <wieser.eric@gmail.com>
2022-02-28 22:25:55 +0000

    Sorry-free!

--- src/geometric_algebra/from_mathlib/even_odd.lean
+++ src/geometric_algebra/from_mathlib/even_odd.lean
@@ -20,6 +20,38 @@ This file is an alternative to the `add_monoid_algebra` approach using `direct_s
 The main result is now in mathlib, as `clifford_algebra.graded_algebra`.
 -/
 
+section to_move
+
+lemma commute.mul_self_sub_mul_self_eq {R} [ring R] {a b : R}(h : commute a b) :
+  a * a - b * b = (a + b) * (a - b) :=
+begin
+  rw [add_mul, mul_sub, mul_sub, h.eq],
+  abel,
+end
+
+lemma commute.mul_self_sub_mul_self_eq' {R} [ring R] {a b : R} (h : commute a b) :
+  a * a - b * b = (a - b) * (a + b) :=
+begin
+  rw [mul_add, sub_mul, sub_mul, h.eq],
+  abel,
+end
+
+def _root_.submodule.to_subalgebra {R A : Type*} [comm_semiring R] [semiring A] [algebra R A]
+  (p : submodule R A)
+  (h_one : (1 : A) ∈ p)
+  (h_mul : ∀ x y, x ∈ p → y ∈ p → x * y ∈ p) : subalgebra R A :=
+{ mul_mem' := h_mul,
+  algebra_map_mem' := λ r, begin
+    rw algebra.algebra_map_eq_smul_one,
+    apply p.smul_mem _ h_one,
+  end,
+  ..p}
+
+@[simp, to_additive] lemma prod.swap_mul {α β} [has_mul α] [has_mul β] (x y : α × β) :
+  prod.swap (x * y) = prod.swap x * prod.swap y := rfl
+
+end to_move
+
 namespace clifford_algebra
 
 variables {R M : Type*} [comm_ring R] [add_comm_group M] [module R M]
@@ -29,15 +61,6 @@ open_locale direct_sum
 
 variables (Q)
 
-def _root_.submodule.to_subalgebra {A : Type*} [semiring A] [algebra R A] (p : submodule R A)
-  (h_one : (1 : A) ∈ p)
-  (h_mul : ∀ x y, x ∈ p → y ∈ p → x * y ∈ p) : subalgebra R A :=
-{ mul_mem' := h_mul,
-  algebra_map_mem' := λ r, begin
-    rw algebra.algebra_map_eq_smul_one,
-    apply p.smul_mem _ h_one,
-  end,
-  ..p}
 
 lemma ι_mul_ι_mem_even_odd_zero (m₁ m₂ : M) :
   ι Q m₁ * ι Q m₂ ∈ even_odd Q 0 :=
@@ -118,81 +141,31 @@ begin
     apply hadd, }
 end
 
-namespace equiv_even
-
-@[reducible]
-def Q' : quadratic_form R (M × R) := (Q.prod $ -@quadratic_form.sq R _)
-
-/-- The unit vector in the new dimension -/
-def e0 : clifford_algebra (Q' Q) := ι (Q' Q) (0, 1)
-
-lemma eq_smul_e0 (r : R) : ι (Q' Q) (0, r) = r • e0 Q :=
-by rw [e0, ←linear_map.map_smul, prod.smul_mk, smul_zero, smul_eq_mul, mul_one]
-
-lemma e0_mul_e0 : e0 Q * e0 Q = -1 :=
-(ι_sq_scalar _ _).trans begin
-  simp,
-end
-
-lemma neg_e0_mul_ι (m : M) : -(e0 Q * ι _ (m, 0)) = ι _ (m, 0) * e0 Q :=
-begin
-  refine neg_eq_of_add_eq_zero ((ι_mul_ι_add_swap _ _).trans _),
-  dsimp [quadratic_form.polar],
-  simp only [add_zero, mul_zero, mul_one, zero_add, neg_zero, quadratic_form.map_zero,
-    add_sub_cancel, sub_self, map_zero, zero_sub],
-end
-
-lemma neg_ι_mul_e0 (m : M) : -(ι _ (m, 0) * e0 Q) = e0 Q * ι _ (m, 0) :=
-begin
-  rw neg_eq_iff_neg_eq,
-  exact neg_e0_mul_ι _ m
-end
-
-/-- The embedding from the smaller algebra into the new larger one. -/
-def to_even : clifford_algebra Q →ₐ[R] clifford_algebra.even (Q' Q) :=
-begin
-  refine clifford_algebra.lift Q ⟨_, λ m, _⟩,
-  { refine linear_map.cod_restrict _ _ (λ m, submodule.mem_supr_of_mem ⟨2, rfl⟩ _),
-    exact (algebra.lmul_left R $ e0 Q).comp ((ι _).comp $ linear_map.inl R M R),
-    rw [subtype.coe_mk, pow_two],
-    exact submodule.mul_mem_mul (linear_map.mem_range_self _ _) (linear_map.mem_range_self _ _), },
-  { ext1,
-    dsimp only [subalgebra.coe_mul, linear_map.cod_restrict_apply, linear_map.comp_apply,
-      algebra.lmul_left_apply, linear_map.inl_apply, subalgebra.coe_algebra_map],
-    rw [←neg_ι_mul_e0] {occs := occurrences.pos [1]},
-    rw [←mul_neg, mul_assoc, ←mul_assoc _ (e0 _), neg_mul, e0_mul_e0, neg_neg, one_mul,
-      ι_sq_scalar],
-    dsimp [Q'],
-    rw [zero_mul, neg_zero, add_zero], }
-end
-
-@[simp]
-lemma to_even_ι (m : M) : (to_even Q (ι Q m) : clifford_algebra (Q' Q)) = e0 Q * ι (Q' Q) (m, 0) :=
-begin
-  rw [to_even, clifford_algebra.lift_ι_apply, linear_map.cod_restrict_apply],
-  refl,
-end
-
-
 notation `↑ₐ` := algebra_map _ (clifford_algebra _)
-
-@[simp, to_additive] lemma prod.swap_mul {α β} [has_mul α] [has_mul β] (x y : α × β) :
-  prod.swap (x * y) = prod.swap x * prod.swap y := rfl
-
 
 variables {A : Type*} [ring A] [algebra R A] (f : M →ₗ[R] M →ₗ[R] A)
 namespace even.lift
 
--- THIS IS FALSE!
 def S : submodule R (M →ₗ[R] A) :=
-{ carrier := {x | ∃ (acc1 : A) (m : M), x = (algebra.lmul_right R acc1).comp (f.flip m)},
-  zero_mem' := sorry,
-  add_mem' := sorry,
-  smul_mem' := sorry }
+submodule.span R {x | ∃ (acc1 : A) (m : M), x = (algebra.lmul_right R acc1).comp (f.flip m)}
 
 def f_fold : M →ₗ[R] (A × (S f)) →ₗ[R] (A × (S f)) :=
-linear_map.mk₂ R (λ m acc, (acc.2 m, ⟨(algebra.lmul_right R acc.1).comp (f.flip m), _, _, rfl⟩))
-  sorry sorry sorry sorry
+linear_map.mk₂ R (λ m acc, (acc.2 m, ⟨(algebra.lmul_right R acc.1).comp (f.flip m),
+  submodule.subset_span $ ⟨_, _, rfl⟩⟩))
+  (λ m₁ m₂ a, prod.ext
+    (linear_map.map_add _ m₁ m₂)
+    (subtype.ext $ linear_map.ext $ λ m₃,
+      show f m₃ (m₁ + m₂) * a.1 = f m₃ m₁ * a.1 + f m₃ m₂ * a.1,
+      by rw [map_add, add_mul]))
+  (λ c m a, prod.ext
+    (linear_map.map_smul _ c m)
+    (subtype.ext $ linear_map.ext $ λ m₃,
+      show f m₃ (c • m) * a.1 = c • (f m₃ m * a.1),
+      by rw [linear_map.map_smul, smul_mul_assoc]))
+  (λ m a₁ a₂, prod.ext rfl
+    (subtype.ext $ linear_map.ext $ λ m₃, mul_add _ _ _))
+  (λ c m a, prod.ext rfl
+    (subtype.ext $ linear_map.ext $ λ m₃, mul_smul_comm _ _ _))
 
 @[simp] lemma fst_f_fold_f_fold  (m₁ m₂ : M) (x : A × (S f)) :
   (f_fold f m₁ (f_fold f m₂ x)).fst = f m₁ m₂ * x.fst := rfl
@@ -206,14 +179,23 @@ def folded_hom
   (hf₂ : ∀ m₁ m₂ m₃, f m₁ m₂ * f m₂ m₃ = Q m₂ • f m₁ m₃) :
   (A × (S f)) →ₗ[R] clifford_algebra Q →ₗ[R] (A × (S f)) :=
 @foldr R _ M _ _ (A × (S f)) _ _ Q (even.lift.f_fold f) $ λ m₂, linear_map.ext $ begin
-  rintro ⟨a, ⟨_, b, m₃, rfl⟩⟩,
+  rintro ⟨a, ⟨g, hg⟩⟩,
   rw [linear_map.comp_apply],
   ext : 2,
   { change f m₂ m₂ * a = Q m₂ • a,
     rw [algebra.smul_def, hf] },
   { ext m₁,
-    change f _ _ * (f _ _ * b) = Q m₂ • (f _ _ * b),
-    rw [←smul_mul_assoc, ←mul_assoc, hf₂] }
+    change f _ _ * g m₂ = Q m₂ • g m₁,
+    apply submodule.span_induction' _ _ _ _ hg,
+    { rintros _ ⟨b, m₃, rfl⟩,
+      change f _ _ * (f _ _ * b) = Q m₂ • (f _ _ * b),
+      rw [←smul_mul_assoc, ←mul_assoc, hf₂] },
+    { change f m₁ m₂ * 0 = Q m₂ • 0,
+      rw [mul_zero, smul_zero] },
+    { rintros x hx y hy ihx ihy,
+      rw [linear_map.add_apply, linear_map.add_apply, mul_add, smul_add, ihx, ihy] },
+    { rintros x hx c ihx,
+      rw [linear_map.smul_apply, linear_map.smul_apply, mul_smul_comm, ihx, smul_comm] } },
 end
 
 end even.lift
@@ -310,6 +292,9 @@ begin
     exact (f.map_mul _ _).trans (this.trans $ (g.map_mul _ _).symm) },
 end
 
+/-- Every algebra morphism from the even subalgebra is in one-to-one correspondence with a
+bilinear map that sends duplicate arguments to the quadratic form, and contracts across
+multiplication. -/
 def even.lift_equiv :
   { f : M →ₗ[R] M →ₗ[R] A //
       (∀ m, f m m = algebra_map R _ (Q m)) ∧
@@ -324,120 +309,5 @@ def even.lift_equiv :
     even.liftₗ_ι Q _ _ _ _ _,
   right_inv := λ F, even.alg_hom_ext Q $ linear_map.ext $ λ m₁, linear_map.ext $ λ m₂,
     even.liftₗ_ι Q _ _ _ _ _ }
-
-instance : module R (clifford_algebra.even Q) :=
-by apply_instance
-
-lemma _root_.commute.mul_self_sub_mul_self_eq {R} [ring R] {a b : R}(h : commute a b) :
-  a * a - b * b = (a + b) * (a - b) :=
-begin
-  rw [add_mul, mul_sub, mul_sub, h.eq],
-  abel,
-end
-
-lemma _root_.commute.mul_self_sub_mul_self_eq' {R} [ring R] {a b : R} (h : commute a b) :
-  a * a - b * b = (a - b) * (a + b) :=
-begin
-  rw [mul_add, sub_mul, sub_mul, h.eq],
-  abel,
-end
-
-/--
-x.fst * f m m = Q m • x.fst
-x.snd m * f m y = Q m • x.snd y
-
-x * ⇑(⇑f m) m = ⇑Q m • x
-0 * ⇑(⇑f m) m = ⇑Q m • 0
-0 * ⇑(⇑f m) x_1 = ⇑Q m • 0
-⇑x m * ⇑(⇑f m) x_1 = ⇑Q m • ⇑x x_1
-
-Basics:
-
-f ⟨0,1⟩ (f ⟨x,0⟩ acc) = ι x * acc
-f ⟨x,0⟩ (f ⟨0,1⟩ acc) = -ι x * acc
-f ⟨x,0⟩ (f ⟨y,0⟩ acc) = ι x * ι y * acc
-f ⟨0,1⟩ (f ⟨0,1⟩ acc) = -acc
-
-Combined
-
-f ⟨x,rx⟩ (f ⟨y,ry⟩ acc) = (ιx + rx) * (ιy - ry) * acc
-
-So
-
-f ⟨x,rx⟩ (f ⟨x,rx⟩ acc) = (ιx + rx)^2 * acc
-                      = (Qx + 2rxιx + rx^2) * acc
-                      = (Qx - rx^2) * acc
-
-f x (f e0 (f e0 (f e0 acc))) = x * x * acc
-f x (f e0 acc) = x * acc
-
-try:
-  f ⟨x,rx⟩ (a1, a2) = ((ι x + r) * a2, (ι x - r) * a1)
-
-check:
-  f ⟨x,rx⟩ (f ⟨x,rx⟩ (a1, a2)) = f ⟨x,rx⟩ ((ι x + r) * a2, (ι x - r) * a1)
-
---/
-def of_even : clifford_algebra.even (Q' Q) →ₐ[R] clifford_algebra Q :=
-begin
-  let f : (M × R) →ₗ[R] (M × R) →ₗ[R] clifford_algebra Q :=
-  linear_map.mk₂ R (λ x y,
-    (ι Q x.fst + algebra_map R _ x.snd) * (ι Q y.fst - algebra_map R _ y.snd))
-      sorry sorry sorry sorry,
-  have hc : ∀ (r : R) (x : clifford_algebra Q), commute (algebra_map _ _ r) x := algebra.commutes,
-  have hm : ∀ m : M × R,
-    ι Q m.1 * ι Q m.1 - algebra_map R _ m.2 * algebra_map R _ m.2 = algebra_map R _ (Q' Q m),
-  { intro m,
-    rw [ι_sq_scalar, ←ring_hom.map_mul, ←ring_hom.map_sub,
-      sub_eq_add_neg, Q', quadratic_form.prod_to_fun, quadratic_form.neg_apply,
-      quadratic_form.sq_to_fun] },
-  refine even.lift (Q' Q) f _ _; dsimp only [f, linear_map.mk₂_apply],
-  { intro m,
-    rw [←(hc _ _).symm.mul_self_sub_mul_self_eq, hm] },
-  { intros m₁ m₂ m₃,
-    rw [←mul_smul_comm, ←mul_assoc, mul_assoc(_ + _), ←(hc _ _).symm.mul_self_sub_mul_self_eq',
-      algebra.smul_def, ←mul_assoc, hm] },
-end
-
-lemma of_even_ι (x y : M × R) :
-  of_even Q (even.ι _ x y) = (ι Q x.1 + algebra_map R _ x.2) * (ι Q y.1 - algebra_map R _ y.2) :=
-even.lift_ι _ _ _ _ _ _
-
-lemma mul_mul_mul_assoc {α} [semigroup α] (a b c d : α) :
-  (a * b) * (c * d) = a * (b * c) * d := by rw [mul_assoc, mul_assoc, mul_assoc]
-
-lemma of_even_comp_to_even :
-  (to_even Q).comp (of_even Q) = alg_hom.id R _ :=
-even.alg_hom_ext _ $ linear_map.ext $ λ m₁, linear_map.ext $ λ m₂, begin
-  ext : 1,
-  change ↑(to_even Q (of_even Q (even.ι (Q' Q) m₁ m₂))) = ι (Q' Q) m₁ * ι (Q' Q) m₂,
-  rw [of_even_ι, alg_hom.map_mul, alg_hom.map_add, alg_hom.map_sub, alg_hom.commutes,
-    alg_hom.commutes, subalgebra.coe_mul, subalgebra.coe_add, subalgebra.coe_sub,
-    to_even_ι, to_even_ι, subalgebra.coe_algebra_map, subalgebra.coe_algebra_map],
-  rw [mul_sub, add_mul, add_mul, ←mul_assoc (algebra_map _ _ _), ←algebra.smul_def,
-      mul_assoc _ _ (algebra_map _ _ _), ←algebra.commutes, ←mul_assoc _ (algebra_map _ _ _),
-      ←algebra.commutes, ←algebra.smul_def,
-      ←neg_ι_mul_e0, neg_mul, mul_mul_mul_assoc, e0_mul_e0, mul_neg,
-      mul_one, neg_mul, neg_neg, sub_eq_add_neg, neg_add],
-  conv_rhs {
-    rw [←prod.fst_add_snd m₁, ←prod.fst_add_snd m₂, linear_map.map_add, linear_map.map_add,
-      eq_smul_e0, eq_smul_e0, mul_add, add_mul, add_mul, smul_mul_smul, e0_mul_e0, smul_neg,
-      ←algebra.algebra_map_eq_smul_one, ring_hom.map_mul, mul_smul_comm, ←neg_e0_mul_ι, smul_neg,
-      ←smul_mul_assoc],
-  },
-end
-
-def even_equiv : clifford_algebra Q ≃ₐ[R] clifford_algebra.even (Q' Q) :=
-alg_equiv.of_alg_hom
-  (to_even Q)
-  (of_even Q)
-  (of_even_comp_to_even Q)
-  (begin
-    -- ext,
-    -- dsimp only [of_even, to_even],
-    sorry
-  end)
-
-end equiv_even
 
 end clifford_algebra
---
+++ src/geometric_algebra/from_mathlib/even_odd_equiv.lean
@@ -0,0 +1,156 @@
+/-
+Copyright (c) 2020 Eric Wieser. All rights reserved.
+Released under MIT license as described in the file LICENSE.
+Authors: Eric Wieser
+-/
+import geometric_algebra.from_mathlib.even_odd
+/-!
+# The isomorphism with the even subalgebra
+
+The main result in this file is `clifford_algebra.equiv_even`
+-/
+lemma mul_mul_mul_assoc {α} [semigroup α] (a b c d : α) :
+  (a * b) * (c * d) = a * (b * c) * d := by rw [mul_assoc, mul_assoc, mul_assoc]
+
+namespace clifford_algebra
+
+variables {R M : Type*} [comm_ring R] [add_comm_group M] [module R M]
+variables (Q : quadratic_form R M)
+
+namespace equiv_even
+
+@[reducible]
+def Q' : quadratic_form R (M × R) := (Q.prod $ -@quadratic_form.sq R _)
+
+/-- The unit vector in the new dimension -/
+def e0 : clifford_algebra (Q' Q) := ι (Q' Q) (0, 1)
+
+lemma eq_smul_e0 (r : R) : ι (Q' Q) (0, r) = r • e0 Q :=
+by rw [e0, ←linear_map.map_smul, prod.smul_mk, smul_zero, smul_eq_mul, mul_one]
+
+lemma e0_mul_e0 : e0 Q * e0 Q = -1 :=
+(ι_sq_scalar _ _).trans begin
+  simp,
+end
+
+lemma neg_e0_mul_ι (m : M) : -(e0 Q * ι _ (m, 0)) = ι _ (m, 0) * e0 Q :=
+begin
+  refine neg_eq_of_add_eq_zero ((ι_mul_ι_add_swap _ _).trans _),
+  dsimp [quadratic_form.polar],
+  simp only [add_zero, mul_zero, mul_one, zero_add, neg_zero, quadratic_form.map_zero,
+    add_sub_cancel, sub_self, map_zero, zero_sub],
+end
+
+lemma neg_ι_mul_e0 (m : M) : -(ι _ (m, 0) * e0 Q) = e0 Q * ι _ (m, 0) :=
+begin
+  rw neg_eq_iff_neg_eq,
+  exact neg_e0_mul_ι _ m
+end
+
+/-- The embedding from the smaller algebra into the new larger one. -/
+def to_even : clifford_algebra Q →ₐ[R] clifford_algebra.even (Q' Q) :=
+begin
+  refine clifford_algebra.lift Q ⟨_, λ m, _⟩,
+  { refine linear_map.cod_restrict _ _ (λ m, submodule.mem_supr_of_mem ⟨2, rfl⟩ _),
+    exact (algebra.lmul_left R $ e0 Q).comp ((ι _).comp $ linear_map.inl R M R),
+    rw [subtype.coe_mk, pow_two],
+    exact submodule.mul_mem_mul (linear_map.mem_range_self _ _) (linear_map.mem_range_self _ _), },
+  { ext1,
+    dsimp only [subalgebra.coe_mul, linear_map.cod_restrict_apply, linear_map.comp_apply,
+      algebra.lmul_left_apply, linear_map.inl_apply, subalgebra.coe_algebra_map],
+    rw [←neg_ι_mul_e0] {occs := occurrences.pos [1]},
+    rw [←mul_neg, mul_assoc, ←mul_assoc _ (e0 _), neg_mul, e0_mul_e0, neg_neg, one_mul,
+      ι_sq_scalar],
+    dsimp [Q'],
+    rw [zero_mul, neg_zero, add_zero], }
+end
+
+@[simp]
+lemma to_even_ι (m : M) : (to_even Q (ι Q m) : clifford_algebra (Q' Q)) = e0 Q * ι (Q' Q) (m, 0) :=
+begin
+  rw [to_even, clifford_algebra.lift_ι_apply, linear_map.cod_restrict_apply],
+  refl,
+end
+
+/-- The embedding from the even subalgebra with an extra dimension into the original algebra. -/
+def of_even : clifford_algebra.even (Q' Q) →ₐ[R] clifford_algebra Q :=
+begin
+  /-
+  Recall that we need:
+   * `f ⟨0,1⟩ ⟨x,0⟩ = ι x`
+   * `f ⟨x,0⟩ ⟨0,1⟩ = -ι x`
+   * `f ⟨x,0⟩ ⟨y,0⟩ = ι x * ι y`
+   * `f ⟨0,1⟩ ⟨0,1⟩ = -1`
+  -/
+  let f : (M × R) →ₗ[R] (M × R) →ₗ[R] clifford_algebra Q :=
+  ((algebra.lmul R (clifford_algebra Q)).to_linear_map.comp
+    $ ((ι Q).comp (linear_map.fst _ _ _)) +
+      (algebra.linear_map R _).comp (linear_map.snd _ _ _)).compl₂
+    (((ι Q).comp (linear_map.fst _ _ _)) - (algebra.linear_map R _).comp (linear_map.snd _ _ _)),
+  have f_apply :
+    ∀ x y, f x y = (ι Q x.1 + algebra_map R _ x.2) * (ι Q y.1 - algebra_map R _ y.2) :=
+    λ x y, rfl,
+  have hc : ∀ (r : R) (x : clifford_algebra Q), commute (algebra_map _ _ r) x := algebra.commutes,
+  have hm : ∀ m : M × R,
+    ι Q m.1 * ι Q m.1 - algebra_map R _ m.2 * algebra_map R _ m.2 = algebra_map R _ (Q' Q m),
+  { intro m,
+    rw [ι_sq_scalar, ←ring_hom.map_mul, ←ring_hom.map_sub,
+      sub_eq_add_neg, Q', quadratic_form.prod_to_fun, quadratic_form.neg_apply,
+      quadratic_form.sq_to_fun] },
+  refine even.lift (Q' Q) f _ _; simp_rw [f_apply],
+  { intro m,
+    rw [←(hc _ _).symm.mul_self_sub_mul_self_eq, hm] },
+  { intros m₁ m₂ m₃,
+    rw [←mul_smul_comm, ←mul_assoc, mul_assoc(_ + _), ←(hc _ _).symm.mul_self_sub_mul_self_eq',
+      algebra.smul_def, ←mul_assoc, hm] },
+end
+
+lemma of_even_ι (x y : M × R) :
+  of_even Q (even.ι _ x y) = (ι Q x.1 + algebra_map R _ x.2) * (ι Q y.1 - algebra_map R _ y.2) :=
+even.lift_ι _ _ _ _ _ _
+
+lemma to_even_comp_of_even :
+  (to_even Q).comp (of_even Q) = alg_hom.id R _ :=
+even.alg_hom_ext _ $ linear_map.ext $ λ m₁, linear_map.ext $ λ m₂, begin
+  ext : 1,
+  change ↑(to_even Q (of_even Q (even.ι (Q' Q) m₁ m₂))) = ι (Q' Q) m₁ * ι (Q' Q) m₂,
+  rw [of_even_ι, alg_hom.map_mul, alg_hom.map_add, alg_hom.map_sub, alg_hom.commutes,
+    alg_hom.commutes, subalgebra.coe_mul, subalgebra.coe_add, subalgebra.coe_sub,
+    to_even_ι, to_even_ι, subalgebra.coe_algebra_map, subalgebra.coe_algebra_map],
+  rw [mul_sub, add_mul, add_mul, ←mul_assoc (algebra_map _ _ _), ←algebra.smul_def,
+      mul_assoc _ _ (algebra_map _ _ _), ←algebra.commutes, ←mul_assoc _ (algebra_map _ _ _),
+      ←algebra.commutes, ←algebra.smul_def,
+      ←neg_ι_mul_e0, neg_mul, mul_mul_mul_assoc, e0_mul_e0, mul_neg,
+      mul_one, neg_mul, neg_neg, sub_eq_add_neg, neg_add],
+  conv_rhs {
+    rw [←prod.fst_add_snd m₁, ←prod.fst_add_snd m₂, linear_map.map_add, linear_map.map_add,
+      eq_smul_e0, eq_smul_e0, mul_add, add_mul, add_mul, smul_mul_smul, e0_mul_e0, smul_neg,
+      ←algebra.algebra_map_eq_smul_one, ring_hom.map_mul, mul_smul_comm, ←neg_e0_mul_ι, smul_neg,
+      ←smul_mul_assoc],
+  },
+end
+
+lemma of_even_comp_to_even :
+  (of_even Q).comp (to_even Q) = alg_hom.id R _ :=
+clifford_algebra.hom_ext $ linear_map.ext $ λ m, begin
+  change of_even Q (to_even Q (ι Q m)) = ι Q m,
+  rw ←subtype.coe_eta (to_even Q (ι Q m)) (to_even Q (ι Q m)).prop,
+  simp_rw to_even_ι,
+  refine (of_even_ι (Q) (0, 1) (m, 0)).trans _,
+  dsimp only,
+  rw [map_one, map_zero, map_zero, sub_zero, zero_add, one_mul],
+end
+
+/-- Any clifford algebra is isomorphic to the even subalgebra of a clifford algebra with an extra
+dimension (that is, with vector space `M × R`), with a quadratic form evaluating to `-1` on that new
+basis vector. -/
+def equiv_even : clifford_algebra Q ≃ₐ[R] clifford_algebra.even (Q' Q) :=
+alg_equiv.of_alg_hom
+  (to_even Q)
+  (of_even Q)
+  (to_even_comp_of_even Q)
+  (of_even_comp_to_even Q)
+
+end equiv_even
+
+end clifford_algebra
