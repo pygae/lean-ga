e0861d04f1b903f7712f5cf015c088ec8698ff67
Eric Wieser <wieser.eric@gmail.com>
2022-05-13 12:19:14 +0000

    use calc

--- src/geometric_algebra/from_mathlib/even_odd_equiv.lean
+++ src/geometric_algebra/from_mathlib/even_odd_equiv.lean
@@ -28,15 +28,20 @@ def Q' : quadratic_form R (M × R) := (Q.prod $ -@quadratic_form.sq R _)
 /-- The unit vector in the new dimension -/
 def e0 : clifford_algebra (Q' Q) := ι (Q' Q) (0, 1)
 
-lemma eq_smul_e0 (r : R) : ι (Q' Q) (0, r) = r • e0 Q :=
-by rw [e0, ←linear_map.map_smul, prod.smul_mk, smul_zero, smul_eq_mul, mul_one]
+/-- The embedding from the existing vector space -/
+def v : M →ₗ[R] clifford_algebra (Q' Q) := (ι (Q' Q)) ∘ₗ linear_map.inl _ _ _
+
+lemma ι_eq_v_add_smul_e0 (m : M) (r : R) : ι (Q' Q) (m, r) = v Q m + r • e0 Q :=
+by rw [e0, v, linear_map.comp_apply, linear_map.inl_apply, ←linear_map.map_smul, prod.smul_mk,
+  smul_zero, smul_eq_mul, mul_one, ←linear_map.map_add, prod.mk_add_mk, zero_add, add_zero]
 
 lemma e0_mul_e0 : e0 Q * e0 Q = -1 :=
-(ι_sq_scalar _ _).trans begin
-  simp,
-end
+(ι_sq_scalar _ _).trans $ by simp
 
-lemma neg_e0_mul_ι (m : M) : -(e0 Q * ι _ (m, 0)) = ι _ (m, 0) * e0 Q :=
+lemma v_sq_scalar (m : M) : v Q m * v Q m = algebra_map _ _ (Q m) :=
+(ι_sq_scalar _ _).trans $ by simp
+
+lemma neg_e0_mul_v (m : M) : -(e0 Q * v Q m) = v Q m * e0 Q :=
 begin
   refine neg_eq_of_add_eq_zero ((ι_mul_ι_add_swap _ _).trans _),
   dsimp [quadratic_form.polar],
@@ -44,11 +49,17 @@ begin
     add_sub_cancel, sub_self, map_zero, zero_sub],
 end
 
-lemma neg_ι_mul_e0 (m : M) : -(ι _ (m, 0) * e0 Q) = e0 Q * ι _ (m, 0) :=
+lemma neg_v_mul_e0 (m : M) : -(v Q m * e0 Q) = e0 Q * v Q m :=
 begin
   rw neg_eq_iff_neg_eq,
-  exact neg_e0_mul_ι _ m
+  exact neg_e0_mul_v _ m
 end
+
+@[simp] lemma e0_mul_v_mul_e0 (m : M) : e0 Q * v Q m * e0 Q = v Q m :=
+by rw [←neg_v_mul_e0, ←neg_mul, mul_assoc, e0_mul_e0, mul_neg_one, neg_neg]
+
+@[simp] lemma reverse_v (m : M) : reverse (v Q m) = v Q m := reverse_ι _
+@[simp] lemma involute_v (m : M) : involute (v Q m) = -v Q m := involute_ι _
 
 @[simp] lemma reverse_e0 : reverse (e0 Q) = e0 Q := reverse_ι _
 @[simp] lemma involute_e0 : involute (e0 Q) = -e0 Q := involute_ι _
@@ -62,21 +73,17 @@ def to_even : clifford_algebra Q →ₐ[R] clifford_algebra.even (Q' Q) :=
 begin
   refine clifford_algebra.lift Q ⟨_, λ m, _⟩,
   { refine linear_map.cod_restrict _ _ (λ m, submodule.mem_supr_of_mem ⟨2, rfl⟩ _),
-    exact (algebra.lmul_left R $ e0 Q).comp ((ι _).comp $ linear_map.inl R M R),
+    exact (algebra.lmul_left R $ e0 Q).comp (v Q),
     rw [subtype.coe_mk, pow_two],
     exact submodule.mul_mem_mul (linear_map.mem_range_self _ _) (linear_map.mem_range_self _ _), },
   { ext1,
     dsimp only [subalgebra.coe_mul, linear_map.cod_restrict_apply, linear_map.comp_apply,
       algebra.lmul_left_apply, linear_map.inl_apply, subalgebra.coe_algebra_map],
-    rw [←neg_ι_mul_e0] {occs := occurrences.pos [1]},
-    rw [←mul_neg, mul_assoc, ←mul_assoc _ (e0 _), neg_mul, e0_mul_e0, neg_neg, one_mul,
-      ι_sq_scalar],
-    dsimp [Q'],
-    rw [zero_mul, neg_zero, add_zero], }
+    rw [←mul_assoc, e0_mul_v_mul_e0, v_sq_scalar] }
 end
 
 @[simp]
-lemma to_even_ι (m : M) : (to_even Q (ι Q m) : clifford_algebra (Q' Q)) = e0 Q * ι (Q' Q) (m, 0) :=
+lemma to_even_ι (m : M) : (to_even Q (ι Q m) : clifford_algebra (Q' Q)) = e0 Q * v Q m :=
 begin
   rw [to_even, clifford_algebra.lift_ι_apply, linear_map.cod_restrict_apply],
   refl,
@@ -119,37 +126,39 @@ lemma of_even_ι (x y : M × R) :
   of_even Q (even.ι _ x y) = (ι Q x.1 + algebra_map R _ x.2) * (ι Q y.1 - algebra_map R _ y.2) :=
 even.lift_ι _ _ _ _
 
-lemma to_even_comp_of_even :
-  (to_even Q).comp (of_even Q) = alg_hom.id R _ :=
-even.alg_hom_ext _ $ linear_map.ext $ λ m₁, linear_map.ext $ λ m₂, begin
-  ext : 1,
-  change ↑(to_even Q (of_even Q (even.ι (Q' Q) m₁ m₂))) = ι (Q' Q) m₁ * ι (Q' Q) m₂,
-  rw [of_even_ι, alg_hom.map_mul, alg_hom.map_add, alg_hom.map_sub, alg_hom.commutes,
-    alg_hom.commutes, subalgebra.coe_mul, subalgebra.coe_add, subalgebra.coe_sub,
-    to_even_ι, to_even_ι, subalgebra.coe_algebra_map, subalgebra.coe_algebra_map],
-  rw [mul_sub, add_mul, add_mul, ←mul_assoc (algebra_map _ _ _), ←algebra.smul_def,
-      mul_assoc _ _ (algebra_map _ _ _), ←algebra.commutes, ←mul_assoc _ (algebra_map _ _ _),
-      ←algebra.commutes, ←algebra.smul_def,
-      ←neg_ι_mul_e0, neg_mul, mul_mul_mul_assoc, e0_mul_e0, mul_neg,
-      mul_one, neg_mul, neg_neg, sub_eq_add_neg, neg_add],
-  conv_rhs {
-    rw [←prod.fst_add_snd m₁, ←prod.fst_add_snd m₂, linear_map.map_add, linear_map.map_add,
-      eq_smul_e0, eq_smul_e0, mul_add, add_mul, add_mul, smul_mul_smul, e0_mul_e0, smul_neg,
-      ←algebra.algebra_map_eq_smul_one, ring_hom.map_mul, mul_smul_comm, ←neg_e0_mul_ι, smul_neg,
-      ←smul_mul_assoc],
-  },
-end
+lemma to_even_comp_of_even : (to_even Q).comp (of_even Q) = alg_hom.id R _ :=
+even.alg_hom_ext (Q' Q) $ linear_map.ext $ λ m₁, linear_map.ext $ λ m₂, subtype.ext $
+  let ⟨m₁, r₁⟩ := m₁, ⟨m₂, r₂⟩ := m₂ in
+  calc  ↑(to_even Q (of_even Q (even.ι (Q' Q) (m₁, r₁) (m₂, r₂))))
+      = (e0 Q * v Q m₁ + algebra_map R _ r₁) * (e0 Q * v Q m₂ - algebra_map R _ r₂) :
+        by rw [of_even_ι, alg_hom.map_mul, alg_hom.map_add, alg_hom.map_sub, alg_hom.commutes,
+             alg_hom.commutes, subalgebra.coe_mul, subalgebra.coe_add, subalgebra.coe_sub,
+             to_even_ι, to_even_ι, subalgebra.coe_algebra_map, subalgebra.coe_algebra_map]
+  ... = e0 Q * v Q m₁ * (e0 Q * v Q m₂) + r₁ • e0 Q * v Q m₂ - r₂ • e0 Q * v Q m₁
+          - algebra_map R _ (r₁ * r₂) :
+        by rw [mul_sub, add_mul, add_mul, ←algebra.commutes, ←algebra.smul_def, ←map_mul,
+               ←algebra.smul_def, sub_add_eq_sub_sub, smul_mul_assoc, smul_mul_assoc]
+  ... = v Q m₁ * v Q m₂ + r₁ • e0 Q * v Q m₂ + v Q m₁ * r₂ • e0 Q + (r₁ • e0 Q) * r₂ • e0 Q :
+        have h1 : e0 Q * v Q m₁ * (e0 Q * v Q m₂) = v Q m₁ * v Q m₂,
+          by rw [←mul_assoc, e0_mul_v_mul_e0],
+        have h2 : -(r₂ • e0 Q * v Q m₁) = v Q m₁ * r₂ • e0 Q,
+          by rw [mul_smul_comm, smul_mul_assoc, ←smul_neg, neg_e0_mul_v],
+        have h3 : - algebra_map R _ (r₁ * r₂) = (r₁ • e0 Q) * r₂ • e0 Q,
+          by rw [algebra.algebra_map_eq_smul_one, smul_mul_smul, e0_mul_e0, smul_neg],
+        by rw [sub_eq_add_neg, sub_eq_add_neg, h1, h2, h3]
+  ... = ι _ (m₁, r₁) * ι _ (m₂, r₂) :
+        by rw [ι_eq_v_add_smul_e0, ι_eq_v_add_smul_e0, mul_add, add_mul, add_mul, add_assoc]
 
 lemma of_even_comp_to_even :
   (of_even Q).comp (to_even Q) = alg_hom.id R _ :=
-clifford_algebra.hom_ext $ linear_map.ext $ λ m, begin
-  change of_even Q (to_even Q (ι Q m)) = ι Q m,
-  rw ←subtype.coe_eta (to_even Q (ι Q m)) (to_even Q (ι Q m)).prop,
-  simp_rw to_even_ι,
-  refine (of_even_ι (Q) (0, 1) (m, 0)).trans _,
-  dsimp only,
-  rw [map_one, map_zero, map_zero, sub_zero, zero_add, one_mul],
-end
+clifford_algebra.hom_ext $ linear_map.ext $ λ m,
+  calc  of_even Q (to_even Q (ι Q m))
+      = of_even Q ⟨_, (to_even Q (ι Q m)).prop⟩ : by rw subtype.coe_eta
+  ... = (ι Q 0 + algebra_map R _ 1) * (ι Q m - algebra_map R _ 0) : begin
+          simp_rw to_even_ι,
+          exact of_even_ι Q _ _,
+        end
+  ... = ι Q m : by rw [map_one, map_zero, map_zero, sub_zero, zero_add, one_mul]
 
 /-- Any clifford algebra is isomorphic to the even subalgebra of a clifford algebra with an extra
 dimension (that is, with vector space `M × R`), with a quadratic form evaluating to `-1` on that new
@@ -171,7 +180,7 @@ begin
   case h_grade0 : r { simp only [alg_hom.commutes, subalgebra.coe_algebra_map, reverse.commutes] },
   case h_grade1 : m {
     simp only [involute_ι, subalgebra.coe_neg, to_even_ι, reverse.map_mul,
-      reverse_ι, reverse_e0, neg_e0_mul_ι, map_neg] },
+      reverse_v, reverse_e0, reverse_ι, neg_e0_mul_v, map_neg] },
   case h_mul : x y hx hy { simp only [map_mul, subalgebra.coe_mul, reverse.map_mul, hx, hy] },
   case h_add : x y hx hy { simp only [map_add, subalgebra.coe_add, hx, hy] },
 end
@@ -197,7 +206,7 @@ even.lift_ι (-Q) _ m₁ m₂
 /-- The even subalgebra of the algebras with quadratic form `Q` and `-Q` are isomorphic.
 
 Stated another way, `𝒞ℓ⁺(p,q,r)` and `𝒞ℓ⁺(q,p,r)` are isomorphic. -/
-@[simps?]
+@[simps]
 def even_equiv_even_neg : clifford_algebra.even Q ≃ₐ[R] clifford_algebra.even (-Q) :=
 alg_equiv.of_alg_hom
   (even_to_neg Q)
