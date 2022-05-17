c5987fdda320b7dbf66eb15cec92a4cedd5c2a5f
Eric Wieser <wieser.eric@gmail.com>
2022-05-17 18:25:00 +0000

    wip

--- src/geometric_algebra/from_mathlib/fold.lean
+++ src/geometric_algebra/from_mathlib/fold.lean
@@ -7,6 +7,7 @@ import linear_algebra.clifford_algebra.basic
 import linear_algebra.bilinear_form
 import linear_algebra.tensor_product
 import linear_algebra.prod
+import linear_algebra.clifford_algebra.grading
 
 /-!
 # Recursive computation rules
@@ -48,5 +49,33 @@ linear_map.congr_fun (alg_hom.map_one _) n
 @[simp] lemma foldr_mul (f : M →ₗ[R] N →ₗ[R] N) (hf) (n : N) (a b : clifford_algebra Q) :
   foldr Q f hf n (a * b) = foldr Q f hf (foldr Q f hf n b) a :=
 linear_map.congr_fun (alg_hom.map_mul _ _ _) n
+
+lemma foldr_induction {P : clifford_algebra Q → Prop}
+  (hr : ∀ r : R, P (algebra_map _ _ r))
+  (h_add : ∀ x y, P x → P y → P (x + y))
+  (h_ι_mul : ∀ m x, P x → P (ι Q m * x)) : ∀ x, P x :=
+-- begin
+--   intro x,
+--   let S : submodule R (clifford_algebra Q) :=
+--   { carrier := set_of P, 
+--     add_mem' := h_add,
+--     smul_mem' := sorry,
+--     zero_mem' := sorry },
+--   let f : M →ₗ[R] S →ₗ[R] S :=
+--   linear_map.mk₂ R (λ m s, ⟨ι Q m * s, h_ι_mul m s s.prop⟩) sorry sorry sorry sorry,
+--   convert (foldr Q f sorry ⟨1, sorry⟩ x).prop,
+--   rw foldr,
+--   dsimp,
+-- end
+begin
+  intro x,
+  have : x ∈ ⊤ := submodule.mem_top, 
+  rw ←supr_ι_range_eq_top at this,
+  apply submodule.supr_induction _ this (λ i x hx, _) _ h_add,
+  { refine submodule.pow_induction_on _ hr h_add (λ x, _) hx,
+    rintro ⟨m, rfl⟩,
+    exact h_ι_mul _ },
+  { simpa only [map_zero] using hr 0}
+end
 
 end clifford_algebra
